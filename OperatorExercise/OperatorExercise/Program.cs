4ce1b60ad0b4beb93a5a84dc59461a78bd346ae9
cmreavis <cmreavis97@gmail.com>
2022-09-14 10:54:16 -0400

    fixes

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿//variables for add, subtract, multiply
+int num1 = 48;
+int num2 = 7;
+
+//variables for division
+int a = 23;
+int b = 7;
+
+//Addition
+
+int num3 = num1 + num2;
+Console.WriteLine(num3);
+
+//Subtraction
+
+//int num3 = num1 - num2;
+//Console.WriteLine(num3);
+
+//Multiplication
+
+//int num3 = num1 * num2;
+//Console.WriteLine(num3);
+
+//Division
+
+//int quotient = a / b;
+//int remainder = a % b;
+
+//Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
---
+++ OperatorExercise2/OperatorExercise2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Area of a Circle
+
+const double pi = 3.14;
+
+//user prompt for radius
+Console.WriteLine("Please input RADIUS:");
+double radius = Convert.ToDouble(Console.ReadLine());
+double CircleArea = (pi * radius * radius);
+
+//output of circle area
+Console.WriteLine($"The area of a circle with your radius is {CircleArea}.");
